e8939a23a610a5dc642233a7827db8395ab9af66
rehema_funani <rehemafunani@gmail.com>
2024-07-13 22:12:24 +0300

    adds podcast

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+// import './App.css';
+import Podcast from "./Podcasts";
+
+function App() {
+  return (
+    <div className="App">
+     <Podcast/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Podcasts/index.js
@@ -0,0 +1,22 @@
+import './index.css'
+
+
+const Podcast = () =>{
+    return(
+<div>
+    <div className='intro'>
+        <div>
+    <img src='/Images/podcast.jpg' alt='' className='img'/></div>
+<div>
+    <p>Join us for the slack podcast where we explore how to leverage power of slack to streamline your workflow,boost productivity,and foster better collaboration within your team.In each episode
+    Whether you're new to Slack or a seasoned pro, this podcast is your guide to mastering the art of workplace communication and taking your team's efficiency to new heights.
+ </p><button>Play Now</button></div>
+
+  
+{/* In each episode, we dive into practical tips and strategies for getting the most out of Slack, from optimizing your workspace settings to integrating powerful apps and automations. You'll hear from productivity experts, Slack power users, and the Slack team themselves as we uncover hidden features and best practices. */}
+    </div>
+</div>
+    )
+}
+export default Podcast
+
