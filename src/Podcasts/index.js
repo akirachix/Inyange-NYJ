f584f27df187b1f058b9e6df436b2b6b6215efd5
rehema_funani <rehemafunani@gmail.com>
2024-07-20 08:54:09 +0300

    adds blogcast

--- src/Podcasts/index.js
+++ src/Podcasts/index.js
@@ -1,29 +1,5 @@
-// import './index.css'
 
-
-// const Podcast = () =>{
-//     return(
-// <div>
-//     <div className='intro'>
-//         <div className='w'>
-//             <h1>Inyange podcast</h1>
-//     </div>
-// <div>
-//     <h2>Politics Unveiled</h2>
-//     <p>This paragraph introduces an upcoming podcast titled "Politics Unveiled," emphasizing its unique approach to discussing politics through in-depth interviews with influential figures. It highlights the podcast's launch date, frequency, and availability on popular platforms, encouraging listeners to subscribe and stay informed. The tone is engaging and informative, aiming to generate excitement and anticipation among potential listeners.
-//  </p><button>Play Now</button></div>
-
-  
-// {/* In each episode, we dive into practical tips and strategies for getting the most out of Slack, from optimizing your workspace settings to integrating powerful apps and automations. You'll hear from productivity experts, Slack power users, and the Slack team themselves as we uncover hidden features and best practices. */}
-//     </div>
-// </div>
-//     )
-// }
-// export default Podcast
-
-
-
-import './index.css'
+import './index.css';
 const Podcast = () => {
     return (
         <div className='intro'>
